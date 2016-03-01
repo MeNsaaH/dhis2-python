c9a4f66f244931610a8ae217848fdb4009ef7f53
Ken Haase <kh@beingmeta.com>
2016-03-01 17:18:13 -0500

    Add ending newline

--- dhis/endpoint.py
+++ dhis/endpoint.py
@@ -10,6 +10,8 @@ class Endpoint:
             setattr(self, item[0], item[1])
         if not self.return_type and self.relpath and self.relpath.endswith('json'):
             self.return_type = 'json'
+        elif not self.return_type:
+            self.return_type='text'
     def __repr__(self):
         if self.name:
             return '<Endpoint %s>' % self.name
